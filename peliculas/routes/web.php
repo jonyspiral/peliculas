ea1cb2e6e921ea8a8d1b86096038f6b7d5a958d4
Kely <kelymiguezflores.km@gmail.com>
2019-11-08 17:09:21 -0300

    2

--- peliculas/routes/web.php
+++ peliculas/routes/web.php
@@ -14,3 +14,16 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/miPrimeraRuta', function () {
+    return 'Creé mi primer ruta en Laravel';
+});
+Route::get('/esPar/{integer}', function ($integer) {
+  if ($integer % 2 ==0){
+    return 'Par';
+  }
+    return 'impar';
+});
+Route::get('/sumar/{n1}/{n2}', function ($n1,$n2) {
+
+    return $n1+$n2;
+});
